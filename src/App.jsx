19c79ea105b63ba7bff28775abca597e2733466f
Madan <madanak0509@gmail.com>
2026-02-09 08:21:37 +0530

    1.31 making the cocktail or menu section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
+import Cocktail from './components/Cocktail.jsx';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
+      <Cocktail />
     </main>
   )
 }
