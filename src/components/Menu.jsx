d9cee88ad173564f1ab03f96ec3eae63786edcd3
Madan <madanak0509@gmail.com>
2026-02-15 10:40:01 +0530

    Enhance Menu component with cocktail navigation and details display

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,25 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { allCocktails, cocktailLists } from '../../constants'
 
 const Menu = () => {
+    const contentRef = useRef();
     const [currentIndex, setCurrentIndex ] = useState(0); 
+    const totalCocktails = allCocktails.length;
+    const goToSlide = (index) => {
+        const newIndex = (index + totalCocktails) % totalCocktails;
+
+        setCurrentIndex(newIndex);
+    }
+    
+    const getCocktailAt = (indexOffset) => {
+        return allCocktails[(currentIndex + indexOffset + totalCocktails) % totalCocktails]
+    }
+
+    const currentCocktail = getCocktailAt(0);
+    const prevCocktail = getCocktailAt(-1);
+    const nextCocktail = getCocktailAt(1);
+
   return (
     <section id='menu' aria-labelledby='menu-heading'>
         <img src="/images/slider-left-leaf.png" alt="left-leaf" id='m-left-leaf'/>
@@ -15,12 +31,38 @@ const Menu = () => {
             {allCocktails.map((cocktail, index) => {
                 const isActive = index === currentIndex;
                 return (
-                    <button key={cocktail.id} className={`${isActive ? 'text-white border-white' : 'text-white/50 border-white/50'}`}>
+                    <button key={cocktail.id} className={`${isActive ? 'text-white border-white' : 'text-white/50 border-white/50'}`} onClick={() => goToSlide(index)}>
                         {cocktail.name}
                     </button>
                 )
             })}
         </nav>
+        <div className="content">
+            <div className="arrows">
+                <button className='text-left' onClick={() => goToSlide(currentIndex - 1)}>
+                    <span>{prevCocktail.name}</span>
+                    <img src="/images/right-arrow.png" alt="right-arrow" aria-hidden="true" />
+                </button>
+                <button className='text-left' onClick={() => goToSlide(currentIndex - 1)}>
+                    <span>{nextCocktail.name}</span>
+                    <img src="/images/left-arrow.png" alt="left-arrow" aria-hidden="true" />
+                </button>
+            </div>
+            <div className="cocktail">
+                <img src={currentCocktail.image} alt="" className='object-contain'/>
+            </div> 
+            <div className="recipe">
+                <div ref={contentRef} className='info'>
+                    <p>Recipe for:</p>
+                    <p id="title">{currentCocktail.name}</p>
+                </div>
+
+                <div className="details">
+                    <h2>{currentCocktail.title}</h2>
+                    <p>{currentCocktail.description}</p>
+                </div>
+            </div>
+        </div>
     </section>
   )
 }
